1174dd3fc64f8a363074ed392cacd4ca098ec06d
jmcordvs <jomanhatiba@gmail.com>
2024-05-12 00:30:56 -0400

    Added an image icon, changed the window title, and added a delta time function for updating the window frame

--- SFMLLinkTest/Game.cpp
+++ SFMLLinkTest/Game.cpp
@@ -1,9 +1,78 @@
 #include "Game.h"
 
+// Static functions
+
+// Initializer functions
+
+// Constructors and Destructors
+
+void Game::fnInitializeWindow()
+{
+	this->sfWindow = new sf::RenderWindow(sf::VideoMode(800, 400), "SFML works!");
+
+	// Set an image icon for the window
+	sf::Image imgIcon;
+
+	if (!imgIcon.loadFromFile("C:\\Users\\ryzen\\Downloads\\imgIcon.png"))
+	{
+		// Error handling, TODO
+	}
+
+	//Get all the properties of the image icon
+	sfWindow->setIcon(imgIcon.getSize().x, imgIcon.getSize().y, imgIcon.getPixelsPtr());
+}
+
 Game::Game()
 {
+	this->fnInitializeWindow();
 }
 
 Game::~Game()
 {
+	delete this->sfWindow;
+}
+
+// Functions
+
+void Game::fnUpdateDeltaTime()
+{
+	//Updates the flDeltaTime variable with the time it takes to update and render 1 frame
+	this->flDeltaTime = this->deltaTimeClock.getElapsedTime().asSeconds();
+}
+
+void Game::fnUpdateSFMLEvents()
+{
+	while (this->sfWindow->pollEvent(this->sfEvent))
+	{
+		if (this->sfEvent.type == sf::Event::Closed)
+			this->sfWindow->close();
+	}
+}
+
+void Game::fnUpdate()
+{
+	this->fnUpdateSFMLEvents();
+}
+
+void Game::fnRender()
+{
+	this->sfWindow->clear();
+
+	this->sfWindow->display();
+
+	this->sfWindow->requestFocus();
+
+	//Set the title of the window, visible in the title bar
+	this->sfWindow->setTitle("SFML Engine v0.1");
+}
+
+void Game::fnRun()
+{
+	while (this->sfWindow->isOpen())
+	{
+		this->fnUpdateDeltaTime();
+		this->fnUpdate();
+		this->fnRender();
+	}
+
 }
--- SFMLLinkTest/Game.h
+++ SFMLLinkTest/Game.h
@@ -1,14 +1,41 @@
-#pragma once
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Audio.hpp>
+#include <SFML\Network.hpp>
 
 class Game
 {
 private:
+	// Variables
+	sf::RenderWindow  *sfWindow;
+	sf::Event		  sfEvent;
+	sf::Clock		  deltaTimeClock;
 
+	float flDeltaTime;
+	
+	// Initialization
+	void fnInitializeWindow();
 
 
 public:
-
+	// Constructors and Destructors
 	Game();
 	virtual ~Game();
+
+	//Creating functions
+	void fnUpdateDeltaTime();
+	void fnUpdateSFMLEvents();
+	void fnUpdate();
+	void fnRender();
+	void fnRun();
 };
 
+#endif
